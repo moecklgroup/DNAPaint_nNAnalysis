6a5c43894cb5bc84429e2bb3470acdd8e29e855f
unknown <chloe.bielawski@yahoo.com>
2024-08-27 13:18:03 +0200

    fix minor bug

---
+++ MultiColorAnalysis_CrossTypesPossible.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 18 13:40:03 2024
+
+@author: Chloe Bielawski
+"""
+
+
+#%% imports
+
+
+from tqdm import tqdm
+from pathlib import Path    
+import datetime
+
+
+import functionsAll as funct
+
+
+
+
+
+
+#%% paths and name dictionary
+
+
+
+# =============================================================================
+# paths to the csv files to analyse
+# 
+# if the csv files ares the same for the points in search of a neighbor 
+# and those that make up the pool of potential neighbors, 
+# then pathLocsPoints = 'path/to/folder'
+# and pathLocsNeighbors = pathLocsPoints
+# =============================================================================
+
+# path to the csv files of the points in search of neigbors
+pathLocsPoints = r'C:\Users\Admin\Desktop\csv_dnapaint'
+# path to the csv files of the points - pool of potential neighbors 
+pathLocsNeighbors = pathLocsPoints
+
+
+
+
+# =============================================================================
+# dictionary of equivalence: names of source files and futur names of new 
+# created files and figures
+# 
+# to update when new lectins are used 
+# =============================================================================
+
+dictionaryNames = {'wga':'R1WGA',  
+                   'sna':'R2SNA', 
+                   'phal':'R3PHAL', 
+                   'aal':'R4AAL', 
+                   'psa':'R5PSA'}
+
+    
+
+
+# =============================================================================
+# histogram parameters
+# =============================================================================
+
+# histogram distances points to nearest neighbors in same channel
+rangeUpSameChannel = 100 #maximum display x axis
+binsizeSameChannel = 0.1 #bin size
+
+# histogram distances points to nearest neighbors in different channel
+rangeUpCrossChannel = 200 #maximum display x axis
+binsizeCrossChannel = 0.2 #binsize
+
+
+
+
+
+
+#%% import localizations for all six channels
+
+# makes 1 dictionary of arrays for the localizations of the points of all the channels
+dictionaryLocalizationsPoints = funct.MultChannelsCallToDict(pathLocsPoints, dictionaryNames)
+
+# if the paths are the same, there is no need to import the files again 
+if pathLocsNeighbors == pathLocsPoints:
+    dictionaryLocalizationsNeighbors = dict(dictionaryLocalizationsPoints)
+
+else:
+    dictionaryLocalizationsNeighbors = funct.MultChannelsCallToDict(pathLocsNeighbors, dictionaryNames)
+
+
+
+
+
+
+
+#%% new folder for analysis data
+
+# =============================================================================
+# crates new folder for the analysis data named as current date if no analysis 
+# before on the same day
+# if the folder already exists - nothing is done
+# =============================================================================
+
+
+pathNewFolder = pathLocsPoints + '/' + str(datetime.date.today())
+if not Path(pathNewFolder).exists():
+    Path(pathNewFolder).mkdir()
+
+
+
+
+
+
+#%% distance to first nearest neighbor for one in all six channels
+
+
+timenow = datetime.datetime.now().strftime("%H-%M-%S-%f_")
+# timestamp in the beginning of all new data files for identification
+# is the same for all files of the same analysis (run of code)
+
+
+
+# =============================================================================
+# parameters for the histogram that can be chaged : 
+    
+#   max x for display of the histogram 
+#         - 'rangeUp' parameter of the 'displayHistFigure' function
+#         => using the variables rangeUpSameChannel and rangeUpCrossChannel
+
+#   size of the bins for the histogram 
+#         - 'binsize' parameter of the 'displayHistFigure' function
+#         => using the variables binsizeSameChannel and binsizeCrossChannel
+# =============================================================================
+
+for i in tqdm(dictionaryLocalizationsPoints.keys()):
+    
+    #calculate the nearest neighbor for every point in that channel in all the others including itself
+    dictionaryDist = funct.nearestNeighborsOneInAll({i:dictionaryLocalizationsPoints[i]}, dictionaryLocalizationsNeighbors)
+    
+    nameCSV = timenow+'DistNN_'+i+'_In_All'
+    if list(dictionaryLocalizationsNeighbors.keys())[0].casefold().find('random') >= 0:
+        nameCSV = nameCSV+'random'
+    
+    #save to csv (in the new folder inside original folder of the raw data)
+    funct.dictionaryToCsv(dictionaryDist, pathNewFolder+'/'+nameCSV+'.csv')
+    
+    
+    
+# =============================================================================
+#     distance to NN of one channel in itself is displayed separatly 
+#     than the crosschannel distances to NN 
+# =============================================================================
+    
+    
+    nameFIG = timenow+'HistDistNN_'+i+'_In_'+list(dictionaryLocalizationsNeighbors.keys())[list(dictionaryLocalizationsPoints.keys()).index(i)]
+    
+    #display hist of distance to NN for channel i in channel i
+    funct.displayHistFigure({k: v for k, v in dictionaryDist.items() if k == list(dictionaryDist.keys())[list(dictionaryLocalizationsPoints).index(i)]}, 
+                            rangeUp=rangeUpSameChannel, #max x of histogram display
+                            binsize=binsizeSameChannel, #histogram binsize
+                            path=pathNewFolder+'/'+nameFIG) 
+    
+    
+    
+    
+    nameFIG = timenow+'HistDistNN_'+i+'_In_All'
+    if list(dictionaryLocalizationsNeighbors.keys())[0].casefold().find('random') >= 0: nameFIG = nameFIG+'random'
+    
+    #display hist of distance to NN for channel i in all others 
+    funct.displayHistFigure({k: v for k, v in dictionaryDist.items() if not k == list(dictionaryDist.keys())[list(dictionaryLocalizationsPoints).index(i)]}, 
+                            rangeUp=rangeUpCrossChannel, #max x of histogram display
+                            binsize=binsizeCrossChannel, #histogram binsize
+                            path=pathNewFolder+'/'+nameFIG) 
+    
+
+    # =============================================================================
+    #     the csv files of distance to NN and figures 
+    #     are automatically saved in a new folder (1/per day) inside the original 
+    #     folder of the point in search of a neighbor (pathLocsPoints)
+    # =============================================================================
+    
+    continue
+    # (for tqdm)
+
+
+
+
+
+
+
+
+# save analysis parameters in txt file
+
+# =============================================================================
+# all the parameters used in the analysis are saved in a txt file
+# the txt files name starts with the same timestamp than the new data files
+# the txt file is saved with the new data files in the folder created for the day
+# =============================================================================
+
+
+parametersfilename = timenow+'Parameters.txt'
+
+#w tells python we are opening the file to write into it
+outfile = open(pathNewFolder + '/' + parametersfilename, 'w')
+ 
+outfile.write('Path to points for which to find neighbors : ' + pathLocsPoints + '\n\n')
+outfile.write('Path to pools of potential neighbors : ' + pathLocsNeighbors + '\n\n')
+outfile.write('Range histogram same channel : 0-' + str(rangeUpSameChannel) + ' (nm) \n\n')
+outfile.write('bin size histogram same channel : ' + str(binsizeSameChannel) + ' (nm) \n\n')
+outfile.write('Range histogram cross channel : 0-' + str(rangeUpCrossChannel) + ' (nm) \n\n')
+outfile.write('bin size histogram cross channel : ' + str(binsizeCrossChannel) + ' (nm) \n\n')
+
+outfile.close() #Close the file when done
+
+
+
+
+
+
---
+++ SimpleClustering.py
@@ -0,0 +1,297 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  6 09:42:58 2024
+
+@author: Chloe Bielawksi
+"""
+
+
+#%% imports
+
+
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import statistics as stat
+import scipy.spatial as spat
+import glob 
+import datetime
+
+
+import functionsAll as funct
+
+
+
+
+
+
+#%% PATH TO THE NECESSARY DATA
+
+# =============================================================================
+# paths to folder containing the csv files to analyse
+# =============================================================================
+
+# path to the csv files of the points to cluster
+pathLocsPoints = '5_colors_csv/2024-07-17_MCF10A_Lectin_DS019/well2/Cell1'
+
+
+
+# =============================================================================
+# dictionary of equivalence names of source files and futur names of new 
+# created files and figures
+# 
+# to update when new lectins are used 
+# =============================================================================
+
+dictionaryNames = {'wga':'R1WGA',  
+                   'sna':'R2SNA', 
+                   'phal':'R3PHAL', 
+                   'aal':'R4AAL', 
+                   'psa':'R5PSA', 
+                   'mannaz':'R6ManNAz'}
+
+
+
+
+# =============================================================================
+# path to nearest neighbors data
+# =============================================================================
+
+pathDistNN = '5_colors_csv/2024-07-17_MCF10A_Lectin_DS019/well2/Cell1/analysis'
+
+
+
+
+
+
+#%% IMPORT DATA CLUSTERING
+
+
+# makes 1 dictionary of arrays for the localizations of the points of all the channels
+dictionaryLocalizations = funct.MultChannelsCallToDict(pathLocsPoints, dictionaryNames)
+
+
+
+
+
+
+
+
+#%% new folder for analysis data 
+
+# =============================================================================
+# crates new folder for the analysis data named as current date if no analysis 
+# before on the same day
+# if the folder already exists - nothing is done
+# =============================================================================
+
+
+pathNewFolder = pathLocsPoints + '/' + str(datetime.date.today())
+if not Path(pathNewFolder).exists():
+    Path(pathNewFolder).mkdir()
+
+
+
+
+
+
+
+#%% IMPORTING NEARSET NEIGHBOR DATA FOR CLUSTERING
+
+
+
+
+# =============================================================================
+# import already calculated distance to NN 
+# =============================================================================
+
+
+dictionaryDistNN = {}
+
+filePaths = np.array(glob.glob(pathDistNN+'/*.csv'))
+
+for i in filePaths:
+    #names of the files only (without extention or path)
+    name = Path(i).stem
+    
+    #import locations (csv)
+    df = pd.read_csv(i)
+    
+    #search for recognisable part in name of the dict and makes new names accordingly
+    #names of lectins and state of randomness
+    for i, j in zip(dictionaryNames.keys(), range(0, len(dictionaryNames.keys()))):
+        if name.casefold().find(i) >= 0:
+            # interseting columns are coordinates => to new array => in dictionary 
+            df = df.iloc[:, j+1:j+2].to_numpy()
+            name = dictionaryNames[i] + 'In' + dictionaryNames[i]
+
+    #add arry to dictionary of localizations 
+    dictionaryDistNN[name] = df
+
+
+
+
+
+
+
+
+#%% PARAMETERS CLUSTERING DBSCAN 1
+
+# =============================================================================
+# calculatiosn of the radius of clusters and min number of points 
+# for each channel in the dictionry of localisations :
+#     - the radius is the radius 90% of nearest neighbors are inside of  
+#     (maximum clustering)
+#     - the min number of points is the mean number of 
+#     neighbors of a point, inside the radius
+#     (it should be changed from the automatic value if it exceeds 20-30)
+# =============================================================================
+
+
+radius = np.empty(len(dictionaryLocalizations)) #radius of clustering
+nb = np.empty(len(dictionaryLocalizations)) #raw nb of points
+
+
+for i, j, k in zip(dictionaryLocalizations.values(), range(0, len(dictionaryLocalizations)), dictionaryDistNN.values()):
+
+    radius[j] = np.percentile(k, 90)  # return nth percentile
+    
+    nb[j] = stat.median(spat.cKDTree(i).query_ball_point(i, r = radius[j], return_length=True))
+    # the min number of points is the median of the number of neighbors
+    # calculated for each poit of the channel
+    
+print(radius, nb)
+
+
+
+nbUsed = nb.copy()
+
+radiusUsed = radius.copy()
+
+
+
+
+
+
+#%% MANUAL CHOICE OF PARAMETERS
+
+# =============================================================================
+# the user can control of the number of points per cluster with the 
+# following arrays
+# (comment the bloc when not used)
+# =============================================================================
+
+# nbUsed = [10, 10, 10, 10, 10]
+
+# radiusUsed = [10, 10, 10, 10, 10]
+
+# print(radiusUsed, nbUsed)
+
+
+
+#%% CHECK ACCURACY OF CLUSERING
+
+# =============================================================================
+# this part is not saved but is a way to check the parameters for the clustering 
+# and the accuracy of the results
+# for that, the clustering is done on a small section of the data for each channels
+# and a representation of the clusters is diplayed. 
+# =============================================================================
+
+for i, j, k in zip(dictionaryLocalizations.values(), range(0, len(dictionaryLocalizations)), dictionaryLocalizations.keys()):
+    # for each of the channels in the dictionary 
+    
+    dataTest = i[(i[:,0] >= stat.mean(i[:,0])) & 
+                 (i[:,0] <= stat.mean(i[:,0])+2000) & 
+                 (i[:,1] >= stat.mean(i[:,1])) & 
+                 (i[:,1] <= stat.mean(i[:,1])+2000)]
+    # sample of the data to analyse 
+
+    # title of the figure including channel and clustering parameters
+    title = ['Precision Parameters ' + k + ' : esp = ' + "{:.3f}".format(radiusUsed[j]) + ', nbmin = ' + str(nbUsed[j])]
+    
+    # clustering with DBSCAN
+    labelsTest = funct.clusteringDBSCAN(dataTest, radiusUsed[j], nbUsed[j]) # 5, 5
+    print(len(dataTest))
+    centroids = funct.calculateCentroids(labelsTest, dataTest) #works
+    
+    # display of the points and centers of clusters
+    funct.displayPointsCentroids({k:dataTest}, {k:centroids}, 30, 30, title)
+
+
+
+
+
+
+
+
+#%% CLUSTERING AND SAVING OF RESULTS
+
+
+timenow = datetime.datetime.now().strftime("%H-%M-%S-%f_")
+# timestamp in the beginning of all new data files for identification
+# is the same for all files of the same analysis (run of code)
+
+
+
+
+
+# =============================================================================
+# clustering of the data and saving of the results as csv files
+# same principle than precedent bloc, but with saving of the data and no display 
+# =============================================================================
+
+for i, j, k in zip(dictionaryLocalizations.values(), range(0, len(dictionaryLocalizations)), dictionaryLocalizations.keys()):
+    # for each of the channels in the dictionary 
+
+    # title of the figure including channel and clustering parameters
+    title = 'Precision Parameters ' + k + ' : esp = ' + "{:.3f}".format(radiusUsed[j]) + ', nbmin = ' + str(nbUsed[j])
+    
+    # clustering with DBSCAN
+    labels = funct.clusteringDBSCAN(i, radiusUsed[j], nbUsed[j]) # 5, 5
+    print(len(i))
+    centroids = funct.calculateCentroids(labels, i) #works
+    
+    # path and name of the new CSV file containing localizations of centroids of clusters
+    fileName = timenow + k + '_centroids'
+    
+    # save CSV
+    # funct.dictionaryToCsv({k + ' - centroids':centroids}, pathName + '.csv')
+    
+    funct.dictionaryToCsv({'x [nm]':centroids[:,0], 'y [nm]':centroids[:,1]}, 
+                          nameCsv = pathNewFolder + '/' + fileName + '.csv')
+
+
+
+
+
+
+
+
+# save analysis parameters in txt file
+
+# =============================================================================
+# all the parameters used in the analysis are saved in a txt file
+# the txt files name starts with the same timestamp than the new data files
+# the txt file is saved with the new data files in the folder created for the day
+# =============================================================================
+
+
+parametersfilename = timenow+'Parameters.txt'
+
+#w tells python we are opening the file to write into it
+outfile = open(pathNewFolder + '/' + parametersfilename, 'w')
+ 
+outfile.write('Clustering method : 90nth percentile \n\n')
+outfile.write('Path to points for which to find neighbors : ' + pathLocsPoints + '\n\n')
+outfile.write('Max radius clusters calculated : ' + str(radius) + '\n\n')
+outfile.write('Max radius clusters used : ' + str(radiusUsed) + '\n\n')
+outfile.write('Min points cluster calculated: ' + str(nb) + '\n\n')
+outfile.write('Min points cluster used : ' + str(nbUsed) + '\n\n')
+
+outfile.close() #Close the file when done
+
+
+
+
+
